b886964dbbe09c652e6f868b7c93e00af64c9170
zell <m.naghizadeh91@gmail.com>
2020-07-29 08:36:33 +0200

    cadstar package from github.com

--- package/cadstar_protocol/cadstar_protocol.mk
+++ package/cadstar_protocol/cadstar_protocol.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-CADSTAR_PROTOCOL_VERSION = rpi-cm3
+CADSTAR_PROTOCOL_VERSION = 1.0.0
 CADSTAR_PROTOCOL_SITE_METHOD = git
-CADSTAR_PROTOCOL_SITE = git@gitlab.com:m.naghizadeh91/ProjectorJSonRPC.git
+CADSTAR_PROTOCOL_SITE = git@github.com:CADstarGmbH/ProjectorJSonRPC.git
 CADSTAR_PROTOCOL_DEPENDENCIES = qt5base
 CADSTAR_PROTOCOL_CONF_OPTS = QT_CONFIG+=network
 CADSTAR_PROTOCOL_INSTALL_STAGING = YES
