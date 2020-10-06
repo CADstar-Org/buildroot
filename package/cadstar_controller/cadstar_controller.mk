37c7857f6343578458320f4ba838e58dc72f98c9
zell <m.naghizadeh91@gmail.com>
2020-10-06 12:42:33 +0100

    new projector application integrated

--- package/cadstar_controller/cadstar_controller.mk
+++ package/cadstar_controller/cadstar_controller.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CADSTAR_CONTROLLER_VERSION = 0.1
+CADSTAR_CONTROLLER_VERSION = 0.2
 CADSTAR_CONTROLLER_SITE = git@github.com:CADstarGmbH/Projector_app_cm3.git
 CADSTAR_CONTROLLER_SITE_METHOD = git
 CADSTAR_CONTROLLER_DEPENDENCIES = qt5base cadstar_protocol
